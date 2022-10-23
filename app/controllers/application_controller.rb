42891180b49f80978348c312500b74acf9177356
jimasan01 <takayuki01200216@gmail.com>
2022-10-23 16:33:24 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_patameters, if: :devise_controller?
+
+  private
+  def configure_permitted_patameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
